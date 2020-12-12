e99f26eb8957126e80a731ca460ec6614fde6c13
Forrly <70975015+Forrly@users.noreply.github.com>
2020-12-12 03:19:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ burger.addEventListener('click', ()=>{
 	burger.classList.toggle('show-menu')
 	topMenu.classList.toggle('show')
 });
+var str = "<h1 ><span style='color:#383838;'>История автогонок</h1>",
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 80);
+}());
